b1449d7c93f4bd711a8a73ff45b6cb25cc2633a8
Harsh joon <harsh.joon@ssinnovations.org>
2023-05-04 16:29:58 +0530

    first version of camera inside fov

--- object_in_fram.cpp
+++ object_in_fram.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include "math.h"
+#include <stdlib.h>
 
 using namespace std;
 
 // TODO
 // - change coordinate struct to vector
 
+#define PI 3.1415926
+
 struct coordinate
 {
-    float x = 0.0;
-    float y = 0.0;
-    float z = 0.0;
+    float x;// = 0.0;
+    float y;// = 0.0;
+    float z;// = 0.0;
 };
 
 struct camera_properties
 {
-    float horizontal_angle = 20;  // in degrees
-    float vertical_angle   = 20;  // in degress
-    float focal_distance   = 2.0; // in cm
+    float horizontal_angle;// = 20;  // in degrees
+    float vertical_angle;//   = 20;  // in degress
+    float focal_distance;//   = 2.0; // in cm
 };
-
 
 bool point_inside_camera_fov( 
      coordinate * camera_coordinate, 
@@ -28,10 +30,12 @@ bool point_inside_camera_fov(
 
 int main(){
     
-    coordinate * camera_coordinate;
-    coordinate * end_effector_coordinate;
+    cout << "program has started." << endl;
 
-    camera_properties * endoscope_camera_properties;
+    coordinate * camera_coordinate = new coordinate;
+    coordinate * end_effector_coordinate = new coordinate;
+
+    camera_properties * endoscope_camera_properties = new camera_properties;
 
     // set camera coordinate
     camera_coordinate->x = 0.0; // in cm
@@ -39,28 +43,35 @@ int main(){
     camera_coordinate->z = 0.0; // in cm
 
     // set end effector coordinate
-    end_effector_coordinate->x = 10.0; // in cm
-    end_effector_coordinate->y = 10.0; // in cm
+    end_effector_coordinate->x = 0.0; // in cm
+    end_effector_coordinate->y = 0.0; // in cm
     end_effector_coordinate->z = 10.0; // in cm
 
-    
+    // set camera properties
+    endoscope_camera_properties->focal_distance = 2.0;
+    endoscope_camera_properties->horizontal_angle = 20.0;
+    endoscope_camera_properties->vertical_angle   = 20.0;
+
+    cout << "End effection inside camera frame: " << point_inside_camera_fov(camera_coordinate, end_effector_coordinate, endoscope_camera_properties) << endl;
+
+    system("pause");
 
     return 0;
 }
-
-
 
 bool point_inside_camera_fov( 
     coordinate * camera_coordinate, 
     coordinate * end_effector_coordinate,
     camera_properties * endoscope_camera_properties  )
 {
-    coordinate * relative;
+    coordinate * relative = new coordinate;
     relative->x = end_effector_coordinate->x - camera_coordinate->x;
     relative->y = end_effector_coordinate->y - camera_coordinate->y;
     relative->z = end_effector_coordinate->z - camera_coordinate->z;
 
     float dist_from_camera =  sqrt( pow( relative->x, 2 ) + pow( relative->y, 2 ) + pow( relative->z, 2 )  );  
+
+    bool inside_camera_frame = false;
 
     bool front_of_camera_check = false;
     bool front_of_camera_focus_check = false;
@@ -77,10 +88,26 @@ bool point_inside_camera_fov(
         
         // here we check if camera is inside frame or not
 
+        // projection vector
+        // coordinate * projection_vector;
+        // projection_vector->z = 0.0;
+        // projection_vector->x
 
+        float half_horizontal_angle;
+        float half_vertical_angle;
 
+        // atan() return angle in radian
+        half_horizontal_angle = atan( relative->x/relative->z );
+        half_vertical_angle   = atan( relative->y/relative->z ); 
 
+        // convert to radian
+        half_horizontal_angle = (half_horizontal_angle*180)/PI  ;
+        half_vertical_angle   = (half_vertical_angle*180)/PI ;
 
+        if( (half_horizontal_angle <= endoscope_camera_properties->horizontal_angle ) && ( half_vertical_angle <= endoscope_camera_properties->vertical_angle ) ){
+            inside_camera_frame = true;
+        }
+
+        return inside_camera_frame;
     }
-
 }
